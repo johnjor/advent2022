67b94a7cad51e9ee6203d5464691bee5e2f59f1d
johnjor <johnjor@cisco.com>
2023-10-30 21:56:55 -0400

    day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -7,14 +7,7 @@ import (
 	"strings"
 )
 
-func main() {
-	filename := os.Args[1]
-	file, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
+func makePart1Map() map[string]int {
 	values := make(map[string]int)
 
 	// A|X = Rock, B|Y = Paper, C|Z = Scissors
@@ -32,6 +25,41 @@ func main() {
 	values["C X"] = 1 + 6 // Rock, Win
 	values["C Y"] = 2 + 0 // Paper, Loss
 	values["C Z"] = 3 + 3 // Scissor, Draw
+
+	return values
+}
+
+func makePart2Map() map[string]int {
+	values := make(map[string]int)
+
+	// A|X = Rock, B|Y = Paper, C|Z = Scissors
+	// X = Loss, Y = Draw, Z = Win
+	// Loss = +0, Draw = +3, Win = +6
+	// Left = Opponent, Right = Outcome
+	values["A X"] = 3 + 0 // Rock, Loss = Scissors
+	values["A Y"] = 1 + 3 // Rock, Draw = Rock
+	values["A Z"] = 2 + 6 // Rock, Win = Paper
+
+	values["B X"] = 1 + 0 // Paper, Loss = Rock
+	values["B Y"] = 2 + 3 // Paper, Draw = Paper
+	values["B Z"] = 3 + 6 // Paper, Win = Scissors
+
+	values["C X"] = 2 + 0 // Scissor, Loss = Paper
+	values["C Y"] = 3 + 3 // Scissor, Draw = Scissors
+	values["C Z"] = 1 + 6 // Scissor, Win = Rock
+
+	return values
+}
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	values := makePart2Map()
 
 	score := 0
 	scanner := bufio.NewScanner(file)
