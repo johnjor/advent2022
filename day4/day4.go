6b94326caac057a49289361d7a17f09558b400f3
johnjor <johnjor@cisco.com>
2023-11-02 14:33:43 -0400

    Day 4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -8,7 +8,28 @@ import (
 	"strings"
 )
 
-func compareValues(line string) int {
+func compareValuesPart1(a int, b int, x int, y int) int {
+	if (a >= x && b <= y) || (a <= x && b >= y) {
+		//fmt.Printf("YES: %v %v\n", elf1, elf2)
+		return 1
+	} else {
+		//fmt.Printf("NO: %v %v\n", elf1, elf2)
+		return 0
+	}
+}
+
+func compareValuesPart2(a int, b int, x int, y int) int {
+	if (a <= x && b >= x) || (a <= y && b >= y) ||
+		(x <= a && y >= a) || (x <= b && y >= b) {
+		//fmt.Printf("YES: %v %v\n", elf1, elf2)
+		return 1
+	} else {
+		//fmt.Printf("NO: %v %v\n", elf1, elf2)
+		return 0
+	}
+}
+
+func processValues(line string) int {
 	split := strings.Split(line, ",")
 	elf1 := strings.Split(split[0], "-")
 	elf2 := strings.Split(split[1], "-")
@@ -33,13 +54,7 @@ func compareValues(line string) int {
 		panic(err)
 	}
 
-	if (a >= x && b <= y) || (a <= x && b >= y) {
-		//fmt.Printf("YES: %v %v\n", elf1, elf2)
-		return 1
-	} else {
-		//fmt.Printf("NO: %v %v\n", elf1, elf2)
-		return 0
-	}
+	return compareValuesPart2(a, b, x, y)
 }
 
 func main() {
@@ -54,7 +69,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := strings.TrimSuffix(scanner.Text(), "\n")
-		sum += compareValues(line)
+		sum += processValues(line)
 	}
 
 	fmt.Println(sum)
