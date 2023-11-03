b82bcdee900983dca9e27632207abba7fd8efd84
johnjor <johnjor@cisco.com>
2023-11-03 10:42:52 -0400

    Day 5 part 1 complete

--- day5/day5.go
+++ day5/day5.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type Stacks map[int]*Stack
@@ -79,6 +82,27 @@ func InitState(stacks Stacks, scanner *bufio.Scanner) {
 
 	stacks[1].Push("N").Push("S").Push("D").Push("C").Push("V").Push("Q").Push("T")
 	stacks[2].Push("M").Push("F").Push("V")
+	stacks[3].Push("F").Push("Q").Push("W").Push("D").Push("P").Push("N").Push("H").Push("M")
+	stacks[4].Push("D").Push("Q").Push("R").Push("T").Push("F")
+	stacks[5].Push("R").Push("F").Push("M").Push("N").Push("Q").Push("H").Push("V").Push("B")
+	stacks[6].Push("C").Push("F").Push("G").Push("N").Push("P").Push("W").Push("Q")
+	stacks[7].Push("W").Push("F").Push("R").Push("L").Push("C").Push("T")
+	stacks[8].Push("T").Push("Z").Push("N").Push("S")
+	stacks[9].Push("M").Push("S").Push("D").Push("J").Push("R").Push("Q").Push("H").Push("N")
+}
+
+func ParseMove(line string) *Move {
+	r := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
+	matches := r.FindStringSubmatch(line)
+	if matches == nil || len(matches) != 4 {
+		panic(fmt.Sprintf("line: %v, matches %v", line, matches))
+	}
+
+	q, _ := strconv.Atoi(matches[1])
+	f, _ := strconv.Atoi(matches[2])
+	t, _ := strconv.Atoi(matches[3])
+
+	return &Move{q, f, t}
 }
 
 func main() {
@@ -94,16 +118,36 @@ func main() {
 
 	InitState(stacks, scanner)
 
-	for i := 1; i <= 9; i++ {
-		v, _ := stacks[i].Peek()
-		fmt.Println(v)
+	//for i := 1; i <= 9; i++ {
+	//	v, _ := stacks[i].Peek()
+	//	fmt.Println(v)
+	//}
+
+	for scanner.Scan() {
+		line := strings.TrimSuffix(scanner.Text(), "\n")
+		if !strings.HasPrefix(line, "move") {
+			continue
+		}
+
+		move := ParseMove(line)
+
+		for i := 1; i <= move.Qty; i++ {
+			v, err := stacks[move.From].Pop()
+			if err != nil {
+				panic(err)
+			}
+			stacks[move.To].Push(v)
+		}
 	}
 
-	//for scanner.Scan() {
-	//	line := strings.TrimSuffix(scanner.Text(), "\n")
-	//	if !strings.HasPrefix(line, "move") {
-	//		continue
-	//	}
-	//
-	//}
+	buf := strings.Builder{}
+	for i := 1; i <= 9; i++ {
+		v, err := stacks[i].Peek()
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteString(v)
+	}
+
+	fmt.Println(buf.String())
 }
